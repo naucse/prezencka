7172a10f3f1bc0fcb9b66c58109c16b1480165fa
Jana Popelková <milujememarcipan@seznam.cz>
2024-02-18 19:18:17 +0100

    NEdaří sa. Nevadí. Všecko néni napoprvé.

--- JanaPopelkova.py
+++ JanaPopelkova.py
@@ -3,4 +3,4 @@ if answer == "ano":
     print("Supr, tož to to možem rozjet ve velikém. Budem sprostí až hamba!")
 else: print("Že ne? \nNevadí, tohle stejně nikdo nečte, tak možem byt sprostí jak ti dlaždiči :-) \nNebo radši dlaždičky.")
 
-# A tu je potřeba neco dopsat, bo revize, ****!
+# A tady je potřeba neco dopsat, bo revize, ****!
